ecf2f82988b53f58d9061e21c7253fda0da00a40
Joshua Loyal <joshua.d.loyal@gmail.com>
2016-09-26 10:03:09 -0400

    added a setup.py file

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+
+PACKAGES = [
+    'mda',
+]
+
+def setup_package():
+    setup(
+        name="MarginalizedDenoisingAutoEncoder",
+        version='0.1.0',
+        description='Python Package for the Marginalized Denoising Autoencoder Algorithm',
+        author='Joshua D. Loyal',
+        url='https://github.com/joshloyal/MarginalizedDenoisingAutoencoder',
+        license='MIT',
+        install_requires=['numpy', 'scipy', 'scikit-learn'],
+        packages=PACKAGES,
+    )
+
+
+if __name__ == '__main__':
+    setup_package()
